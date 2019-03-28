fd6346879860a62a482174833741c30918f90a0d
Orange <ttghost@126.com>
2019-03-29 01:12:05 +0800

    update eslintrc for global

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
   "extends": "airbnb-base",
   "rules": {
     "semi": [0],
@@ -13,5 +13,10 @@
       "exports": "never",
       "functions": "ignore"
     }]
+  },
+  "globals": {
+    // 设置全局变量，false为不可重新赋值
+    "ato": false,
+    "PromiseError": false
   }
 }
