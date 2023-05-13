369eec11cd193ac4ebfff7aa7d3457b26c2914ef
chengzi <ttghost@126.com>
2023-05-13 14:06:22 +0800

    feat: xx

--- src/app.js
+++ src/app.js
@@ -1,8 +1,3 @@
-// const Koa = require("koa");
-// const env = require("./config/env")();
-// const router = require("./modules");
-// const middlewares = require("./middlewares");
-// const DBUtil = require("./utils/db");
 import globalUtil from "./utils/global.js";
 import ChengApp from "./ChengApp/Application.js";
 
@@ -10,24 +5,9 @@ class App extends ChengApp {
   constructor() {
     super();
     console.log("constructor: ", this.app);
-    // globalUtil(global);
-
-    // init database
-    // const sqlFiles = ["user"];
-    // new DBUtil().$initDB(sqlFiles);
-
-    // const app = new Koa();
-
-    // 加载中间件
-    // middlewares(app);
-
-    // router
-    // app.use(router.routes());
-    // app.use(router.allowedMethods());
-
-    // app.listen(env.PORT);
-
-    // console.log(`server is runing on ${env.PORT} port`);
+  }
+  main() {
+    console.log("main");
   }
   configCreated(app) {
     console.log("configCreated: ", app);
