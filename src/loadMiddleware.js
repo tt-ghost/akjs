c7b4a423e5019af03bc50b9952d488d7b9e67ba0
chengzi <ttghost@126.com>
2023-05-21 16:38:58 +0800

    feat: test

--- src/loadMiddleware.js
+++ src/loadMiddleware.js
@@ -4,15 +4,19 @@ import { getProjectPath, isFunction } from "./utils.js";
 export default async (app) => {
   const { config } = app;
   const middleware = config.middleware || [];
-  middleware.forEach(async (mid) => {
-    const middlewarePath = path.resolve(
-      getProjectPath(),
-      `src/middleware/${mid}.js`
-    );
-    const middlewareModule = await import(middlewarePath);
+  const promises = [];
+  middleware.forEach((mid) => {
+    promises.push(async () => {
+      const middlewarePath = path.resolve(
+        getProjectPath(),
+        `src/middleware/${mid}.js`
+      );
+      const middlewareModule = await import(middlewarePath);
 
-    if (middlewareModule && isFunction(middlewarePath.default)) {
-      app.use(await middlewareModule.default(app));
-    }
+      if (middlewareModule && isFunction(middlewarePath.default)) {
+        app.use(await middlewareModule.default(app));
+      }
+    });
   });
+  await Promise.all(promises);
 };
--- src/utils.js
+++ src/utils.js
@@ -8,29 +8,21 @@ export const dirname = () => {
 
 export const getProjectPath = () => process.cwd();
 
-export const load = (file) => {
-  const filePath = path.resolve(getProjectPath(), file);
-  return import(filePath);
-};
-
-export const isFunction = fn => typeof fn === 'function'
+export const isFunction = (fn) => typeof fn === "function";
 
 // 转成大驼峰
-export const bigCamelCase = str => {
-  const trimed = str.trim()
-  const sym = '[-_.\\s]+'
+export const bigCamelCase = (str) => {
+  const trimed = str.trim();
+  const sym = "[-_.\\s]+";
   return trimed
-    .replace(new RegExp('^' + sym), '')
-    .replace(new RegExp(sym + '$'), '')
-    .replace(new RegExp(`(${sym}[a-zA-Z])`, 'g'), (a) => {
-      return a.replace(new RegExp(sym), '').toUpperCase()
+    .replace(new RegExp("^" + sym), "")
+    .replace(new RegExp(sym + "$"), "")
+    .replace(new RegExp(`(${sym}[a-zA-Z])`, "g"), (a) => {
+      return a.replace(new RegExp(sym), "").toUpperCase();
     })
-    .replace(/^([a-zA-Z])/, (a) => a.toUpperCase())
-}
+    .replace(/^([a-zA-Z])/, (a) => a.toUpperCase());
+};
 // 转成小驼峰
-export const smallCamelCase = str => {
-  const trimed = str.trim()
-  const sym = '[-_.\\s]+'
-  return bigCamelCase(str)
-    .replace(/^([a-zA-Z])/, (a) => a.toLowerCase())
-}
+export const smallCamelCase = (str) => {
+  return bigCamelCase(str).replace(/^([a-zA-Z])/, (a) => a.toLowerCase());
+};
